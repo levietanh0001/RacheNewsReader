bd16b6fd25e2ff91a5d851d7c7bf904679875915
levietanh_bi9035 <anhlv.bi9035@st.usth.edu.vn>
2021-10-19 10:33:22 +0700

    import gson

---
+++ app/src/main/java/com/example/android/rachenewsreader/NewsRecycleAdapter.java
@@ -0,0 +1,8 @@
+package com.example.android.rachenewsreader;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.ViewGroup;
+import java.util.List;
+
